6e4d9376cf4e32f3a333090003d56578691c3c7d
asadayoo <ahmadassadf428@gmail.com>
2025-07-04 16:04:34 +0700

    feat: mengubah atau menambahkan router

--- src/components/ComplaintApp.jsx
+++ src/components/ComplaintApp.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./Navbar";
 import HomePage from "../pages/HomePage";
+import CategoryPage from "../pages/complaints/CategoryPage";
 import Footer from "./Footer";
 
 export default function ComplaintApp() {
-	return (
-		<>
-			<Navbar searchField logOutButton />
-			<HomePage />
-			<Footer />
-		</>
-	);
+  return (
+    <Router>
+      <Navbar searchField logOutButton />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/category" element={<CategoryPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
