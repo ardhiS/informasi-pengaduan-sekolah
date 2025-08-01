499ecde3de36800931efa8a5a06c3572a97bb8f9
Ardhi <ardhi333@gmail.com>
2025-08-01 18:32:58 +0700

    coba

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,37 @@
-import { useState, useEffect, useMemo } from "react";
-import { BrowserRouter } from "react-router-dom";
-import ComplaintApp from "./components/ComplaintApp";
-import { ThemeProvider } from "./contexts/ThemeContext";
+import { useState, useEffect, useMemo } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import ComplaintApp from './components/ComplaintApp';
+import { ThemeProvider } from './contexts/ThemeContext';
 // import "./styles/style.css";
-import { AuthProvider } from "./contexts/AuthContext";
-
+import { AuthProvider } from './contexts/AuthContext';
+// ....
 export default function App() {
-	const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
 
-	function toggleTheme() {
-		setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
-	}
+  function toggleTheme() {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  }
 
-	useEffect(() => {
-		document.documentElement.setAttribute("data-bs-theme", theme);
-		localStorage.setItem("theme", theme);
-	}, [theme]);
+  useEffect(() => {
+    document.documentElement.setAttribute('data-bs-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
 
-	const themeContextValue = useMemo(
-		() => ({
-			theme,
-			toggleTheme,
-		}),
-		[theme]
-	);
+  const themeContextValue = useMemo(
+    () => ({
+      theme,
+      toggleTheme,
+    }),
+    [theme]
+  );
 
-	return (
-		<BrowserRouter>
-			<ThemeProvider value={themeContextValue}>
-				<AuthProvider>
-					<ComplaintApp />
-				</AuthProvider>
-			</ThemeProvider>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <ThemeProvider value={themeContextValue}>
+        <AuthProvider>
+          <ComplaintApp />
+        </AuthProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 }
